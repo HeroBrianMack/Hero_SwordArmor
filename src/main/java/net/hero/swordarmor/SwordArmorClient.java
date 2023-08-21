39523cf4ff898ba0e471f32f4fb75b9b896b5ab4
Hero <herostillplays@gmail.com>
2023-08-21 15:07:58 -0400

    Initial commit

---
+++ src/main/java/net/hero/swordarmor/SwordArmorClient.java
@@ -0,0 +1,10 @@
+package net.hero.swordarmor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SwordArmorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
